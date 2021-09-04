0ef6d7aa6d89d31c7b8a1c28fa268952e344f045
Niv Nagli <itsniv@gmail.com>
2021-09-04 16:20:48 +0300

    search-movie-list added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fetchData = async searchTerm => { // 1) in description
+const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'd9835cc5',
@@ -6,13 +6,58 @@ const fetchData = async searchTerm => { // 1) in description
     }
   });
 
+  if (response.data.Error) {
+    return [];
+  }
+
   return response.data.Search;
 };
 
-const input = document.querySelector('input');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
-const onInput = async event => { // 2) in description
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+
+  if(!movies.length) {  // if the mov
+    dropdown.classList.remove('is-active');
+    return;
+  }
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+    option.classList.add('dropdown-item');
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');  // remove dropdown
+      input.value = movie.Title;  // write the selected movie name inside the input
+    });
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
+
+    resultsWrapper.appendChild(option);
+  }
 };
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
