5d67c0346ddf8127096ff1f2f95965d5e60c2d04
Niv Nagli <itsniv@gmail.com>
2021-09-05 12:54:38 +0300

    edit structure

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-
-
-createAutoComplete(
-  {root: document.querySelector('.autocomplete'),
-   fetchData : async (searchTerm) => {
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
+    `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
     const response = await axios.get('http://www.omdbapi.com/', {
       params: {
         apikey: 'd9835cc5',
         s: searchTerm
       }
     });
-  
+
     if (response.data.Error) {
       return [];
     }
-  
-    return response.data.Search;
-  },
-  renderOption: (movie) => {
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-    return `
-    <img src="${imgSrc}" />
-    ${movie.Title}
-  `;
-  },
-  onOptionSelect: async (movie) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-        apikey: 'd9835cc5',
-        i: movie.imdbID
-      }
-    });
-  
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
-  },
-  getValue : (movie) => {return movie.Title;}
 
+    return response.data.Search;
+  }
 });
 
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'd9835cc5',
+      i: movie.imdbID
+    }
+  });
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
 
 const movieTemplate = movieDetail => {
   return `
